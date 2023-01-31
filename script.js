7871cf0c1c86254ceed25a3d2cf647d2659ec3d5
Paulo Pinto <gomescasador@gmail.com>
2023-01-31 15:55:38 +0000

    Terminado visual e script.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var tarefa = document.querySelector('#itarefa')
-alert(tarefa.value)
+var tarefa = document.querySelector('#tarefa')
+var cadastrada = document.querySelector('#tarefas_cadastradas')
+
+
+function cadastrar() {
+    var criarTarefa = document.createElement('li')
+    criarTarefa.innerText = tarefa.value
+    cadastrada.appendChild(criarTarefa)
+}
+
